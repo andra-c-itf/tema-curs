1642cdf4d45af8a24c2b27ce509a8aee9f8a25fe
Andra C <andraelena17.ac@gmail.com>
2022-05-08 18:01:55 +0300

    fixed "

--- tema-3/js.js
+++ tema-3/js.js
@@ -6,26 +6,26 @@ let activity3 = 'learning';
 console.log (premise + activity3 + ' the ' + activity1 + ' of ' + activity2 + '.');
 
 
-let a = 'Jane Smith has $12,450 in her bank account.\n';
-let b = '$12,450 is a lot of money.\n';
-let c = 'Jane Smith plans to buy a Toyota Yaris.\n';
-let d = 'Jane Smith will use her $12,450 to buy a Toyota Yaris.\n';
-let e = '$12,450 is a lot of money for a Toyota Yaris.\n';
+let a = 'Jane Smith has $12,450 in her bank account.';
+let b = '$12,450 is a lot of money.';
+let c = 'Jane Smith plans to buy a Toyota Yaris.';
+let d = 'Jane Smith will use her $12,450 to buy a Toyota Yaris.';
+let e = '$12,450 is a lot of money for a Toyota Yaris.';
 let brake = '=============================================';
 
 let firstPageElement = document.getElementById ('paragraph1');
 let firstP = document.createElement('p');
-firstP.innerHTML = a + "<br/>"+ b + "<br/>" + c + "<br/>"+ d + "<br/>"+ e +"<br/>"+ "<br/>"+ brake;
+firstP.innerHTML = a + '<br/>' + b + '<br/>' + c + '<br/>' + d + '<br/>' + e +'<br/>' + '<br/>' + brake;
 firstPageElement.appendChild(firstP);
 
 let thePageElement2 = document.getElementById ('paragraph2');
 let secondP = document.createElement('p');
-secondP.innerHTML = b + "<br/>"+ e + "<br/>" + c + "<br/>"+ a + "<br/>"+ d +"<br/>"+ "<br/>"+ brake;
+secondP.innerHTML = b + '<br/>' + e + '<br/>' + c + '<br/>' + a + '<br/>' + d +'<br/>' + '<br/>'+ brake;
 thePageElement2.appendChild(secondP);
 
 let thePageElement3 = document.getElementById ('paragraph3');
 let thirdP = document.createElement('p');
-thirdP.innerHTML = c + "<br/>"+ e + "<br/>" + d + "<br/>"+ a + "<br/>"+ b;
+thirdP.innerHTML = c + '<br/>' + e + '<br/>' + d + '<br/>' + a + '<br/>' + b;
 thePageElement3.appendChild(thirdP);
 
 
