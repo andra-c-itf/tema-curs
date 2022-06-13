2ac37bdaf24ae4201ea454d989686acdbe7a4bc0
Andra C <andraelena17.ac@gmail.com>
2022-06-13 19:59:44 +0300

    tema7

---
+++ tema-7/div.js
@@ -0,0 +1,20 @@
+let onClickedDiv = function(){
+    let clickedDiv = document.querySelector('.divDimensions');
+    clickedDiv.style.bottom = '0';
+    clickedDiv.style.right = '0';
+    clickedDiv.style.margin = '0px 50px 50px 0px'
+}
+
+let myDiv = document.querySelector('.divDimensions');
+myDiv.addEventListener('click', onClickedDiv);
+
+/*let mediaQuery = window.matchMedia('(min-width: 500px)');
+if (mediaQuery.matches) {
+    let onClickedDiv500 = function(){
+        let clickedDiv500 = document.querySelector('.divDimensions');
+        clickedDiv500.style.bottom = '0';
+        clickedDiv500.style.right = '0';
+        clickedDiv500.style.margin = '0px 5px 5px 0px'
+    }
+    myDiv.addEventListener('click', onClickedDiv500);
+}*/
